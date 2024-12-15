e084256a950e41b121d1e39fd1dab52346c1983d
applem2dudu <35301543+githubdudu@user.noreply.github.com>
2024-12-15 19:58:55 +1300

    Added EuclideanGraph.java. Ref:Exercises 4.1.37 Algorithms, 4th edition by Robert., Kevin.

---
+++ proj3/byow/DungeonMap/EuclideanGraph.java
@@ -0,0 +1,58 @@
+package byow.DungeonMap;
+
+import edu.princeton.cs.algs4.Graph;
+
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.util.HashMap;
+import java.util.Map;
+
+class EuclideanGraph {
+    private Map<Point2D, Integer> map; // Point2D -> index
+    private Point2D[] keys; // index -> Point2D
+    private Graph graph; // the graph
+
+    public EuclideanGraph(Iterable<Line2D> lines, int N) {
+        map = new HashMap<>();
+        graph = new Graph(N);
+
+        for (Line2D line : lines) {
+            Point2D p1 = line.getP1();
+            Point2D p2 = line.getP2();
+            if (!map.containsKey(p1)) {
+                map.put(p1, map.size());
+            }
+            if (!map.containsKey(p2)) {
+                map.put(p2, map.size());
+            }
+            graph.addEdge(map.get(p1), map.get(p2));
+        }
+
+        // inverted indexOf to get string keys in an array
+        keys = new Point2D[map.size()];
+        for (Point2D p : map.keySet()) {
+            keys[map.get(p)] = p;
+        }
+
+    }
+
+    public boolean contains(Point2D s) {
+        return map.containsKey(s);
+    }
+
+    public int indexOf(Point2D s) {
+        return map.get(s);
+    }
+
+    public Point2D pointOf(int v) {
+        return keys[v];
+    }
+
+    public Graph graph() {
+        return graph;
+    }
+
+    public void show() {
+
+    }
+}
